d5ba169f2ed0f3ac60ea57dc2a2516e91eed0d9f
Kien Do <kienmarkdo@gmail.com>
2021-02-05 15:17:40 -0500

    Fourth commit Second.java
    Fourth commit

--- Second.java
+++ Second.java
@@ -5,5 +5,7 @@ public class Second {
 		System.out.println("Second commit with different email");
 
 		System.out.println("Third commit with different email using GitHub");
+
+		System.out.println("Fourth commit using GitHub. Note that the prev commit was actually with Git Bash.")
 	}
 }
